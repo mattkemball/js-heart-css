0e0643b149d099668e685bffe985698e49d1eb8a
mattkemball <matthewkemball@Matthews-MacBook-Pro.local>
2016-03-08 16:30:26 -0500

    slug list
    added all the required java script

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('header').addClass('masthead');
+$('h1').removeClass('big-heading').addClass('logo');
+$('main').addClass('.wrapper');
+$('p:first-child').addClass('intro');
+$('h2').addClass('slug-head');
+$('ul').addClass('slug-list');
+$('img').addClass('slug-img');
+$('ul li:first-child').addClass('slug-list-first');
+$('ul li:last-child').addClass('slug-list-last');
+$('dl').addClass('classification');
+$('dt').removeClass('big-label');
